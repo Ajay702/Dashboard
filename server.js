6454f9ad75300f4415768d10dfce3e4043cb124d
Ajay702 <74392659+Ajay702@users.noreply.github.com>
2024-06-30 02:58:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const connectDb = require("./config/dbConnection");
 const DataModel = require("./models/DataModel");
-const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
-const cors = require('cors'); 
 
 
-connectDb()
-
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "default-src 'self'; img-src 'self' https://dashboard-server-iota.vercel.app; script-src 'self' 'unsafe-inline'");
-  next();
-});
 app.use(cors());
+
+
+connectDb();
+
+
 app.get('/', async (req, res) => {
-    const data = await DataModel.find();
-    res.json(data);
+    try {
+        const data = await DataModel.find();
+        res.json(data);
+    } catch (err) {
+        console.error("Error fetching data:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
