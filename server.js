500e31da33dc5a8335632afc1cf8893d85535e88
Ajay702 <74392659+Ajay702@users.noreply.github.com>
2024-06-30 13:12:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,26 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors'); 
 
-connectDb()
+
+connectDb();
+
 
 app.use(cors());
-app.get('/', async (req, res) => {
-    const data = await DataModel.find();
-    res.json(data);
-});
 
+
+const getData = async (req, res) => {
+    try {
+        const data = await DataModel.find();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+app.get('/', getData);
+
+// Set up server to listen on specified port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
