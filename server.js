7622368858e84a6658beb7de075f496a9f455642
Ajay702 <74392659+Ajay702@users.noreply.github.com>
2024-06-30 12:58:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors'); 
 
-
-connectDb()
+connectDb();
 
 app.use(cors());
+
 app.get('/', async (req, res) => {
-    const data = await DataModel.find();
-    res.json(data);
+    try {
+        const limit = parseInt(req.query.limit) || 10; 
+        const data = await DataModel.find().limit(limit);
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
 });
 
 const port = process.env.PORT || 5000;
